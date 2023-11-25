47a7cc2856fdce96cde25a75d8f44ef7fe1fe96b
arturo.iafrate <arturo.iafrate@gmail.com>
2023-11-25 16:06:40 +0100

    Added uptime condition flow

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import Homey from 'homey';
+import { convertToUnraidUptime, objStringify } from './utils/utilites';
 
 class UnraidRemoteApp extends Homey.App {
 
@@ -7,6 +8,31 @@ class UnraidRemoteApp extends Homey.App {
   }
 
   async _initConditionCards(): Promise<void> {
+    //System Uptime
+    const systemUptimeCondition = this.homey.flow.getConditionCard('uptime-condition');
+    systemUptimeCondition.registerRunListener(async (args) => {
+      if(!args.years && !args.days && !args.hours && !args.minutes){
+        throw new Error('Threshold is not set');
+      }
+      const argsUptime = convertToUnraidUptime(args.years, args.days, args.hours, args.minutes);
+      const devices: Homey.Device[] = this.homey.drivers.getDriver('unraid-driver').getDevices();
+      for (const device of devices) {
+        if (device.hasCapability('uptime')) {
+          const uptime: number | undefined = await device.getCapabilityValue('uptime');
+          if (!uptime) {
+            throw new Error('Uptime is not set');
+          }
+          this.log(objStringify('Uptime', {
+            uptime: uptime,
+            argsUptime: argsUptime
+          }));
+          if (Number(uptime) > argsUptime) {
+            return Promise.resolve(true);
+          }
+        }
+      }
+      return Promise.resolve(false);
+    });
     //CPU Usage
     const cpuUsageCondition = this.homey.flow.getConditionCard('cpu-usage-condition');
     cpuUsageCondition.registerRunListener(async (args) => {
--- utils/utilites.ts
+++ utils/utilites.ts
@@ -8,3 +8,7 @@ export const objStringify = (title : string, obj : any) : string => {
 export const isNonEmpty = (str : string) : boolean => {
     return typeof str === 'string' && str.length > 0;
 };
+
+export const convertToUnraidUptime = (years: number=0, days: number=0, hours: number=0, minutes: number=0) : number => {
+    return (years * 365 * 24) + (days * 24) + hours +(minutes/100);
+};
